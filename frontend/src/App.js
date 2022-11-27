71c3687f025eaa81ec5919eb8864703431f984e9
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-11-27 01:16:38 -0700

    Added user registration and signin forms to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Header';
 import Photos from './components/pages/Photos';
+import UserAuthentication from './components/pages/UserAuthentication';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(-1);
+
+  let toDisplay;
+  if(loggedIn) {
+    toDisplay = "";
+  } else {
+    toDisplay = <UserAuthentication setLoggedIn={setLoggedIn} setUserId={setUserId}/>;
+  }
+
   return (
     <div className="App">
       <Header />
-      <Photos />
+      {toDisplay}
     </div>
   );
 }
---
+++ frontend/src/components/pages/UserAuthentication.js
@@ -0,0 +1,99 @@
+import axios from "axios";
+import { useState } from "react";
+
+const UserAuthentication = ({setLoggedIn, setUserId}) => {
+    const [showSignIn, setShowSignIn] = useState(true);
+
+    let display;
+    if(showSignIn) {
+        display = <LoginForm setShowSignIn={setShowSignIn} setLoggedIn={setLoggedIn} setUserId={setUserId} />
+    } else {
+        display = <SignUpForm setShowSignIn={setShowSignIn} />
+    }
+
+    return (
+        <div className="main-content-container flex justify-center items-center">
+            {display}
+        </div>
+    );
+}
+
+const LoginForm = ({setShowSignIn, setLoggedIn, setUserId}) => {
+    const logIn = e => {
+        e.preventDefault();
+        let inputs = document.querySelectorAll('input');
+        let reqBody = {};
+        reqBody.username = inputs[0].value;
+        reqBody.password = inputs[1].value;
+
+        axios.post("/api/auth/signin", reqBody)
+            .then(res => {
+                setUserId(Number(res.data));
+                setLoggedIn(true);
+            })
+            .catch(err => {
+                inputs.forEach(input => input.value = "");
+            });
+    }
+
+    const navToRegistration = () => {
+        setShowSignIn(false);
+    }
+
+    return (
+        <div>
+            <form className="user-auth-form">
+                <label for="username">Username:</label>
+                <input type="text" required id="username" />
+                <label for="password">Password:</label>
+                <input type="password" required id="password" />
+                <button onClick={logIn}>Log In</button>
+            </form>
+
+            <div className="change-form-prompt">
+                <span>Don't have an account?</span><button onClick={navToRegistration} className="change-form-btn">Register</button>
+            </div>
+        </div>
+    );
+}
+
+const SignUpForm = ({setShowSignIn}) => {
+    const register = e => {
+        e.preventDefault();
+        let inputs = document.querySelectorAll('input');
+        let reqBody = {};
+        reqBody.username = inputs[0].value;
+        reqBody.password = inputs[1].value;
+
+        axios.post("/api/auth/signup", reqBody)
+            .then(res => {
+                alert(res.data);
+            })
+            .catch(err => {
+                alert(err.response.data);
+                inputs.forEach(input => input.value = "");
+            });
+    }
+
+    const navToLoginPage = () => {
+        setShowSignIn(true);
+    }
+
+    return (
+        <div>
+            <form className="user-auth-form">
+                <label for="username">Username:</label>
+                <input type="text" required id="username" />
+                <label for="password">Password:</label>
+                <input type="password" required id="password" />
+                <button onClick={register}>Register</button>
+            </form>
+
+            <div className="change-form-prompt">
+                <span>Already have an account?</span><button onClick={navToLoginPage} className="change-form-btn">Sign In</button>
+            </div>
+        </div>
+    )
+}
+
+export default UserAuthentication;
